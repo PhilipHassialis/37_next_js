b163188a26aed515043d071800aa4e5183983403
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-11-08 07:52:06 +0200

    basic routing in next

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function contact() {
+    return (
+        <div>
+            <h2>Contact page</h2>
+            <p>philip.alexander.hassialis@gmail.com</p>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function index() {
+    console.log("*********** RUNNING INDEX");
     return (
         <div>
             <h1>My Index page</h1>
