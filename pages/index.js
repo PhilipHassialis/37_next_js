7b39e11be39afcaf4b6b7a663a7b3f0cdc7127c8
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-11-10 17:54:57 +0200

    introducing getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,42 @@
 import React from "react";
 
-export default function index() {
+const index = () => {
     console.log("*********** RUNNING INDEX");
     return (
         <div>
             <h1>My Index page</h1>
         </div>
     );
-}
+};
+
+index.getInitialProps = async () => {
+    console.log("GETINITIALPROPS 2.0 - FUNCTION COMPONENT");
+    return {};
+};
+
+export default index;
+
+// import React, { Component } from "react";
+
+// export default class index extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     componentDidMount() {
+//         // console.log("FETCHING YOUR DATA - COMPOENENTDIDMOUNT");
+//     }
+
+//     static async getInitialProps() {
+//         console.log("FETCHING YOUR DATA - GETINITIALPROPS");
+//         return {};
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>My Index page</h1>
+//             </div>
+//         );
+//     }
+// }
