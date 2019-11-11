12bd8918f0c4c554883269fd7f23f14c708701fd
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-11-11 23:04:26 +0200

    withRouter higher order component

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { withRouter } from "next/router";
 
-const post = ({ id }) => {
-    console.log(`IN POST #${id}`);
-    return <h1>Post #{id}</h1>;
+const post = props => {
+    // console.log(`IN POST #${id}`);
+    return <h1>Post #{props.router.query.id}</h1>;
 };
 
-post.getInitialProps = async ({ query }) => {
-    console.log("POST GET INITIAL PROPS", query);
+// post.getInitialProps = async ({ query }) => {
+//     console.log("POST GET INITIAL PROPS", query);
 
-    return query;
-};
+//     return query;
+// };
 
-export default post;
+export default withRouter(post);
